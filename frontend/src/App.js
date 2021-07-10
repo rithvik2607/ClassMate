60f09c09dae3c756ce5249cae7d699ca55e62378
Niti Shah <emailtonitishah@gmail.com>
2021-07-10 17:44:26 +0530

    Signup Page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,17 @@ import LoginPage from "./components/login.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from './Component'
+import Signup from './Component/Signup/Signup.js'
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <Switch>
-        {/* <Route
-          exact
-          path="/"
-          render={() => {
-            return;
-          }}
-        >
-          <LoginPage />
-        </Route> */}
         <Route path="/" component={LoginPage}/>
+      </Switch>
+      <Switch>
+        <Route path="/" component={Signup}/>
       </Switch>
     </Router>
   );
