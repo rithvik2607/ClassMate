5512074957573f9fbcb66e8e56c3e53ebe225eff
Mayank1403 <mayank1403@gmail.com>
2021-07-10 18:46:42 +0530

    Removed services and implemented direct axios

--- frontend/src/Component/LoginPage/Login.jsx
+++ frontend/src/Component/LoginPage/Login.jsx
@@ -1,44 +1,72 @@
-import React, { useState } from 'react';
-import FormDataService from "../../services/serviceForm";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import { apiBaseURL } from "../../Config";
+import { useHistory } from "react-router";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-    const submit = (e) => {
-        e.preventDefault();
-        if (!email || !password) alert("Email or Password is blank");
-        else{
-            var data = {
-                email: email,
-                password: password
-            }
-            FormDataService.sendLoginDetail(data);
-            setEmail("")
-            setPassword("")
-        }
-
+  const submit = (e) => {
+    e.preventDefault();
+    if (!email || !password) alert("Email or Password is blank");
+    else {
+      var data = {
+        email: email,
+        password: password,
+      };
+      axios
+        .post(`${apiBaseURL}/login`, data)
+        .then((res) => {
+          setEmail("");
+          setPassword("");
+          history.push("/dashboard");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    return (
-        <>
-            <div className="container w-50">
-                <h3 className="text-center py-5">
-                    Login Page
-                </h3>
-                <form onSubmit={submit}>
-                    <div className="mb-3">
-                        <label for="email" className="form-label">Email address</label>
-                        <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} className="form-control" id="email" />
-                    </div>
-                    <div className="mb-3">
-                        <label for="password" className="form-label">Password</label>
-                        <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} className="form-control" id="password" />
-                    </div>
-                    
-                    <button type="submit" className="btn">Sign In</button>
-                </form>
-            </div>
-        </>
-    )
+  };
+  return (
+    <>
+      <div className="container w-50">
+        <h3 className="text-center py-5">Login Page</h3>
+        <form onSubmit={submit}>
+          <div className="mb-3">
+            <label for="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="form-control"
+              id="email"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="form-control"
+              id="password"
+            />
+          </div>
+
+          <button type="submit" className="btn">
+            Sign In
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
--- frontend/src/services/serviceForm.js
+++
@@ -1,9 +0,0 @@
-import http from "../http-common";
-
-class FormDataService {
-  sendLoginDetail(data) {
-    return http.post("/checkLogin", data);
-  }
-}
-
-export default new FormDataService();
