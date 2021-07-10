dd8d3f929c3a7dc431976c1317b9c337f935569b
Mayank1403 <mayank1403@gmail.com>
2021-07-10 21:16:41 +0530

    Meet Page Done

--- frontend/src/Component/MeetPage/MeetPage.jsx
+++ frontend/src/Component/MeetPage/MeetPage.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MeetPage.module.css";
 import { TextField, Button } from "@material-ui/core";
+import { useAlert } from "react-alert";
+import { useRouteMatch } from "react-router-dom";
 
 function MeetPage() {
+  const notification = useAlert();
+  const [name, setName] = useState("");
+
+  const match = useRouteMatch("/meet/:id");
+
+  const handleJoinMeet = () => {
+    if (name === "")
+      notification.show("Name Cannot be blank", { type: "error" });
+    notification.show(match.params.id);
+    window.location.href = `https://us04web.zoom.us/j/${match.params.id}`;
+  };
+
   return (
     <div className={styles.container}>
-      <div>
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <Button variant="contained" color="primary">
+      <div className={styles.innerContainer}>
+        <h1>Join Meeting</h1>
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          className={styles.field}
+          fullWidth
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Button variant="contained" color="primary" onClick={handleJoinMeet}>
           Join
         </Button>
       </div>
