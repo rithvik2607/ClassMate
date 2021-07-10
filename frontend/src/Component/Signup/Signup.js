0c0ba166ded98251e4d97255487e0444ba72fa27
Mayank1403 <mayank1403@gmail.com>
2021-07-10 21:50:52 +0530

    Sign Up Page Done

--- frontend/src/Component/Signup/Signup.js
+++ frontend/src/Component/Signup/Signup.js
@@ -1,38 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Signup.module.css";
 import { TextField, Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { apiBaseURL } from "../../Config";
 
 function Signup() {
   const history = useHistory();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Clicked on Submit");
-
-    history.push("/dashboard");
+    const data = {
+      email,
+      password,
+    };
+    axios
+      .post(`${apiBaseURL}/users/signup`, data)
+      .then((res) => {
+        setEmail("");
+        setPassword("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div className={styles.container}>
-      <form className={styles.registration} onSubmit={handleSubmit}>
-        <h1 className={styles.heading}>Sign Up</h1>
-        <TextField
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          type="email"
-        />
-        <TextField
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          type="password"
-        />
-        <Button variant="contained" color="primary" type="submit">
+      <div className={styles.innerContainer}>
+        <h1 className={styles.login}>Sign Up</h1>
+        <div className={styles.fieldsContainer}>
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            type="email"
+            className={styles.field}
+            fullWidth
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type="password"
+            className={styles.field}
+            fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
           Submit
         </Button>
-      </form>
+      </div>
     </div>
   );
 }
