6b9c8a1f19fc3ec5ccdeb6f381ceeb7121dda722
Sai Rithvik Ayithapu <rithvikayithapu@gmail.com>
2021-07-11 01:14:05 +0530

    Attendace route added

--- Backend/src/routes/attendance.js
+++ Backend/src/routes/attendance.js
@@ -1,3 +1,45 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const Attendance = require('../models/attendance');
+const router = express.Router();
 
+router.post("/join", async (req, res) => {
+    const { name, meetId } = req.body;
+
+    try {
+        let meeting = await Meeting.findOne({
+            meetId
+        });
+        if(!meeting) {
+            return res.status(400).json({
+                message: "This meet does not exist"
+            });
+        }
+
+        const email = meeting.email;
+
+        let attend = await Attendance.findOne({
+            meetId
+        });
+        if(attend) {
+            attend.students.push(name);
+
+            await attend.save();
+        } else {
+            const attendance = new Attendance({
+                meetId,
+                email,
+            });
+    
+            attendance._id = mongoose.Types.ObjectId();
+            attendance.students.push(name);
+    
+            await attendance.save();
+        }
+    } catch(err) {
+        console.error(err.message);
+        return res.status(500).send("Error in saving");
+    }
+});
+
+module.exports = router;
