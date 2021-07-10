edc4a6174e77fba1c2c7f6bea86c772a882ac3d3
muditsinghal <muditsinghal.191cs138@nitk.edu.in>
2021-07-10 16:23:06 +0530

    Scrapped and back to idea 0

--- twilio test/send_sms.py
+++ twilio test/send_sms.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
-import xlrd
+import pandas as pd
 
 loc = ("C:\\Users\\mudit\\Documents\\JPMC CFG\\data_dummy_cfg.xlsx")
 
-wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0)
+df = pd.read_excel(loc)
+# print(str(df["Phone Number"][4]))
 
 # Your Account SID from twilio.com/console
 account_sid = "AC5178f9bda8dde22feafc2c5d058f319f"
@@ -13,10 +13,10 @@ auth_token  = "dece1dae6fe09cb80ee25b11fa398958"
 
 client = Client(account_sid, auth_token)
 
-for i in range(1,5):
+for i in range(0,5):
     message = client.messages.create(
-        to="+91"+sheet.cell_value(1,i), 
+        to="+91"+str(df["Phone Number"][i]), 
         from_="+13372430938",
-        body="test cfg"+sheet.cell_value(0,i))
+        body="test cfg"+str(df["Name"][i]))
 
-print("work")
+print("working")
